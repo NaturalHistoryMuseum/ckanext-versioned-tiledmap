65cf0e956d681c7cbaf54646bd20caeba08f32d6
vagrant <vagrant@precise64.(none)>
2014-01-28 12:32:30 +0000

    Added map controller

---
+++ ckanext/map/controllers/map.py
@@ -0,0 +1,52 @@
+from pylons import config
+import ckan.logic as logic
+import ckan.lib.base as base
+import ckan.model as model
+import ckan.plugins as p
+from ckan.common import OrderedDict, _, json, request, c, g, response
+from ckan.lib.jsonp import jsonpify
+import logging
+
+log = logging.getLogger(__name__)
+
+render = base.render
+abort = base.abort
+redirect = base.redirect
+
+NotFound = logic.NotFound
+NotAuthorized = logic.NotAuthorized
+ValidationError = logic.ValidationError
+get_action = logic.get_action
+
+class MapController(base.BaseController):
+    """
+    Controlled for displaying map tiles
+    """
+
+    def get(self, resource_id):
+
+        """
+        View a map
+        :param id:
+        :param resource_id:
+        :return: string
+        """
+        c.filters = request.POST.items()
+
+        # TODO: Q matches _full_text
+
+        context = {'model': model, 'session': model.Session, 'user': c.user or c.author}
+
+        # Try & get the resource
+        try:
+            c.resource = get_action('resource_show')(context, {'id': resource_id})
+        except NotFound:
+            abort(404, _('Resource not found'))
+        except NotAuthorized:
+            abort(401, _('Unauthorized to read resources'))
+
+        return 'STUFF'
+
+
+
+
