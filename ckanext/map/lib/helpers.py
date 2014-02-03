bd103553f154248ee94c4338b8f53d26aa4da9d3
Andy Allan <andy@gravitystorm.co.uk>
2014-02-03 11:38:14 +0000

    Add some helpers for the SQL wrangling.

---
+++ ckanext/map/lib/helpers.py
@@ -0,0 +1,50 @@
+# From http://stackoverflow.com/questions/5631078/sqlalchemy-print-the-actual-query
+
+def interpolateQuery(statement, bind=None):
+    """
+    print a query, with values filled in
+    for debugging purposes *only*
+    for security, you should always separate queries from their values
+    please also note that this function is quite slow
+    """
+    import sqlalchemy.orm
+    if isinstance(statement, sqlalchemy.orm.Query):
+        if bind is None:
+            bind = statement.session.get_bind(
+                    statement._mapper_zero_or_none()
+            )
+        statement = statement.statement
+    elif bind is None:
+        bind = statement.bind
+
+    dialect = bind.dialect
+    compiler = statement._compiler(dialect)
+    class LiteralCompiler(compiler.__class__):
+        def visit_bindparam(
+                self, bindparam, within_columns_clause=False,
+                literal_binds=False, **kwargs
+        ):
+            return super(LiteralCompiler, self).render_literal_bindparam(
+                    bindparam, within_columns_clause=within_columns_clause,
+                    literal_binds=literal_binds, **kwargs
+            )
+
+    compiler = LiteralCompiler(dialect, statement)
+    return compiler.process(statement)
+
+
+# From the sqlalchemy documentation itself
+
+from sqlalchemy import func
+from sqlalchemy.types import UserDefinedType
+
+class Geometry(UserDefinedType):
+    def get_col_spec(self):
+        return "GEOMETRY"
+
+    def bind_expression(self, bindvalue):
+        return func.ST_GeomFromText(bindvalue, type_=self)
+
+    def column_expression(self, col):
+        return func.ST_AsText(col, type_=self)
+
