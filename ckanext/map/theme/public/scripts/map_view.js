e4588aa80120ddd58e141936fba5fd28dd169db4
Alice Heaton <a.heaton@nhm.ac.uk>
2014-04-11 17:20:17 +0100

    Add pointInfo plugin to give information on click. For now this adds a marker (with pulsating animation :)) on the clicked point, but does not yet update the details tab.

--- ckanext/map/theme/public/scripts/map_view.js
+++ ckanext/map/theme/public/scripts/map_view.js
@@ -169,7 +169,8 @@ my.NHMMap = Backbone.View.extend({
     // Set up the plugins available to the map. These are assigned during redraw.
     this.plugins = {
       'tooltipInfo': new my.TooltipPlugin(this, this.map_info.plugin_options['tooltipInfo']),
-      'tooltipCount': new my.TooltipPlugin(this, this.map_info.plugin_options['tooltipCount'])
+      'tooltipCount': new my.TooltipPlugin(this, this.map_info.plugin_options['tooltipCount']),
+      'pointInfo': new my.PointInfoPlugin(this, this.map_info.plugin_options['pointInfo'])
     }
   },
 
@@ -400,6 +401,145 @@ my.DrawShapeControl = L.Control.Draw.extend({
 });
 
 /**
+ * PointInfoPlugin
+ *
+ * Plugin used to display information about a clicked point
+ */
+my.PointInfoPlugin = function(view, options){
+  /**
+   * Enable this plugin
+   */
+  this.enable = function(){
+    this.grid = null;
+    // todo: enable detail tab
+  }
+
+  /**
+   * Disable this plugin
+   */
+  this.disable = function(){
+    // todo: remove detail tab
+    // remove handlers
+  }
+
+  /**
+   * Remove event handlers
+   */
+  this._disable_event_handlers = function(){
+    if (this.grid !== null){
+      this.grid.off('click', $.proxy(this, "_on_click"));
+    }
+  }
+
+  /**
+   * redraw hanlder
+   */
+  this.redraw = function(layers){
+    // todo: handle click events
+    this._disable_event_handlers();
+    this.layers = layers;
+    this.grid = layers['grid'];
+    this.grid.on('click', $.proxy(this, "_on_click"));
+
+    // todo: should we handle clicks outside the map, and remove the marker?
+    // todo: should we handler the escape key and remove the marker ?
+  }
+
+  /**
+   * click handler
+   */
+  this._on_click = function(props){
+    if (typeof this.animation !== 'undefined'){
+      if (this.animation_restart){
+        clearTimeout(this.animation_restart);
+      }
+      $(this.animation).stop();
+    }
+    if (this.layers['_point_info_plugin']){
+      view.map.removeLayer(this.layers['_point_info_plugin']);
+      view.map.removeLayer(this.layers['_point_info_plugin_1']);
+    }
+    if (props && props.data){
+      // Find coordinates. The values in props.latlng is the mouse position, not the point position -
+      // however it helps us know if we have clicked on a point that is wrapped around the world.
+      var lat = props.data.lat;
+      var lng = props.data.lng;
+      if (props.latlng.lng > 180){
+        // Tricky because the clicking might not be within the same 360 block at the center of the marker. We must
+        // test for this eventuality.
+        lng = lng + Math.floor((props.latlng.lng - lng)/360)*360;
+        if (props.latlng.lng - lng > 180){
+          lng = lng + 360;
+        }
+      } else if (props.latlng.lng < -180){
+        lng = lng - Math.floor((lng - props.latlng.lng)/360)*360;
+        if (lng - props.latlng.lng > 180){
+          lng = lng - 360;
+        }
+      }
+      // Highlight the point
+      this.layers['_point_info_plugin'] = L.circleMarker([lat, lng], {
+        radius: 4, /* Ideally the same as the cartoCSS version */
+        stroke: true,
+        color: '#FFF',
+        weight: 1,
+        fill: true,
+        fillColor: '#00F',
+        opacity: 1,
+        fillOpacity: 1,
+        clickable: false
+      });
+      // Create pulse layer
+      this.layers['_point_info_plugin_1'] = L.circleMarker([lat, lng], {
+        radius: 1,
+        stroke: false,
+        fill: true,
+        fillColor: '#FFF',
+        fillOpacity: 1,
+        clickable: false
+      });
+      this._animate(this.layers['_point_info_plugin_1']);
+      // Add the layers in order
+      view.map.addLayer(this.layers['_point_info_plugin_1']);
+      view.map.addLayer(this.layers['_point_info_plugin']);
+    } else {
+      delete this.layers['_point_info_plugin'];
+    }
+    // Todo: update details tab
+  }
+
+  /**
+   * Animate
+   */
+  this._animate = function(layer){
+    var plugin = this;
+    this.animation = {
+      radius: 1,
+      fillOpacity: 1
+    };
+    $(this.animation).animate({
+      radius: 20,
+      fillOpacity: 0
+    }, {
+      duration: 1000,
+      easing: 'swing',
+      step: function(value, fx){
+        layer.setRadius(this.radius);
+        layer.setStyle({fillOpacity: this.fillOpacity});
+      },
+      complete: function(){
+        plugin.animation_restart = setTimeout(function(){
+          plugin.animation_restart = false;
+          $.proxy(plugin, '_animate')(layer);
+        }, 1000);
+      }
+    });
+  }
+
+};
+
+
+/**
  * TooltipPlugin
  *
  * Plugin used to add a tooltip on point hover
@@ -497,15 +637,17 @@ my.TooltipPlugin = function(view, options){
    * Mouseout handler
    */
   this._mouseout = function(){
-    this.$el.stop().animate({
-      'opacity': 0
-    }, {
-      duration: 100,
-      complete: function(){
-        $(this).html('');
-        $(this).css('display', 'none');
-      }
-    });
+    if (this.$el){
+      this.$el.stop().animate({
+        'opacity': 0
+      }, {
+        duration: 100,
+        complete: function(){
+          $(this).html('');
+          $(this).css('display', 'none');
+        }
+      });
+    }
   }
 
   /**
