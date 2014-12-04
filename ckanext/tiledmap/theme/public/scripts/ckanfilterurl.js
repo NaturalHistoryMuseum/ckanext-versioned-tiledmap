3afbf7ea5f887ed0872a3d8ad08baeac463b749b
benscott <ben@benscott.co.uk>
2014-12-04 19:04:23 +0000

    _f filed filters should be removed

--- ckanext/tiledmap/theme/public/scripts/ckanfilterurl.js
+++ ckanext/tiledmap/theme/public/scripts/ckanfilterurl.js
@@ -6,6 +6,10 @@ this.tiledmap = this.tiledmap || {};
  * Class used to parse and create URLs with a query string parameter 'filter' build as 'name:value|....'
  */
 my.CkanFilterUrl = function(input_url){
+
+  // List of filters that are used internally - for example, for field filters (_f)
+  internal_filters = ['_f']
+
   /**
    * initialize
    *
@@ -85,12 +89,14 @@ my.CkanFilterUrl = function(input_url){
    * Set a filter value on the URL. If the value evaluates to false, the filter is removed
    */
   this.set_filter = function(name, value){
-    if (!value){
-      this.remove_filter(name);
-    } else {
-      this.add_filter(name, value);
-    }
+    if($.inArray(name, internal_filters) == -1){
+        if (!value){
+          this.remove_filter(name);
+        } else {
+          this.add_filter(name, value);
+        }
 
+    }
     return this;
   }
 
